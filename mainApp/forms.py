d808ed4de79d5a4a0a1ccf91d305fa7a7a3c49ba
Andrey Borisov <i78901234567890@gmail.com>
2017-12-11 22:19:20 +0300

    Попробовал создавать дерриктории, какая то фигня, путь пишется верный, но создается все до последнего пути

--- mainApp/forms.py
+++ mainApp/forms.py
@@ -3,5 +3,3 @@ from django import forms
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
     file = forms.FileField(label='File' )
-    file1 = forms.FileField(label='File2')
-    file2 = forms.FileField(label='File1')
--- mainApp/views.py
+++ mainApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import os
 from django.views import View
+from add_images import settings
 
 from mainApp.forms import NameForm
 from mainApp.models import Files_from_client
@@ -22,6 +23,15 @@ def handle_uploaded_file(f, name):
     with open('media/{0}/{1}'.format(name, f), 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+def creat_dir(name1):
+    interim_name = os.path.join(settings.BASE_DIR, "add_images/test_token/")
+    dir_template_name = os.path.join(interim_name, name1)
+    page_number = os.path.join(dir_template_name, "page_number")
+    os.makedirs(interim_name, exist_ok=True)
+    os.makedirs(os.path.dirname(dir_template_name), exist_ok=True)
+    os.makedirs(os.path.dirname(dir_template_name), exist_ok=True)
+#    os.makedirs(os.path.dirname(page_number), exist_ok=True, mode=0o777)
+    print(dir_template_name)
 
 class Test(View):
     def get(self, request):
@@ -43,5 +53,6 @@ class Test(View):
             # new_bd.save()
             # #Передача принятого файла в обработчик для перезаписи в текстовик
             handle_uploaded_file(request.FILES['file'], infor['your_name'])
-
+            name = request.FILES['file'].name
+            creat_dir(name)
             return render(request, 'test.html')
